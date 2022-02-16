56d38abd6e3e7b642e3e316b97ea7838ff7726b9
Muhamad Dedi Yusup <mdyusup45@gmail.com>
2022-02-16 13:09:33 +0700

    PHP crud

--- tambah_data.php
+++ tambah_data.php
@@ -1,3 +1,4 @@
+//memanggil header
 <?php include 'header.php'; ?>
 <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -28,6 +29,8 @@
     </div>
     </div>
     <?php
+
+    //cek apakah tombol submit sudah ditekan atau belum
     include 'koneksi.php';
     if (isset($_GET['submit'])) {
         $nama = $_GET['nama'];
