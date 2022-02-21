fade0e2575a8494d82387679b64d7e29f6c65dcd
Muhamad Dedi Yusup <mdyusup45@gmail.com>
2022-02-21 15:26:28 +0700

    Update tambah_data.php
    update

--- tambah_data.php
+++ tambah_data.php
@@ -1,4 +1,4 @@
-//memanggil header
+//pemanggilan header
 <?php include 'header.php'; ?>
 <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
